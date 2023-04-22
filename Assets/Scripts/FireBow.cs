9fc3bbfb856eee1dd59d053d649474f7e55fbf7b
Max-Manausa <maxmanausa@gmail.com>
2023-04-22 08:52:14 -0400

    Animation works on the bow now

--- Assets/Scripts/FireBow.cs
+++ Assets/Scripts/FireBow.cs
@@ -3,23 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.XR;
+
 public class FireBow : MonoBehaviour
 {
     [SerializeField] private GameObject arrowSpawnPoint;
-    //[SerializeField] public Animator bowAnimation;
-    
+    [SerializeField] public Animator bowAnimation;
+
     public float fireRate = 0.5f;
     public GameObject arrowPrefab;
     public float arrowForce = .1f;
-    
+
 
     public void OnButtonSmash()
     {
-        //bowAnimation.Play();
-        
+        DrawBack();
+        Invoke("ShootArrow",.5f);
+    }
+
+    public void DrawBack()
+    {
+        bowAnimation = GetComponent<Animator>();
+        bowAnimation.enabled = true;
+        bowAnimation.Play("GetShotBoi");
+    }
+
+    public void ShootArrow()
+    {
         Transform arrowSpawnPos = arrowSpawnPoint.transform;
-        
+
         GameObject arrow = Instantiate(arrowPrefab, arrowSpawnPos.position, arrowSpawnPos.rotation);
         Rigidbody arrowRb = arrow.GetComponent<Rigidbody>();
         arrowRb.AddForce(arrowSpawnPos.forward * arrowForce, ForceMode.Impulse);

681c5e159f25d619733f50dc264d40f8432e27d5
Max-Manausa <maxmanausa@gmail.com>
2023-05-14 18:25:44 -0400

    Current state: Audio not playing on start for some reason

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -4,49 +4,31 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> objectsToCheck;
     [SerializeField] private AudioSource audioSource;
-
-    public bool musicPlaying = true;
-    public bool allObjectsInactive = true;
-    public bool isMuted = false;
+    [SerializeField] private GameObject videoScreen;
     
-    public void Check()
-    {
-        foreach (GameObject obj in objectsToCheck)
-        {
-            if (obj.activeSelf)
-            {
-                if (musicPlaying)
-                {
-                    audioSource.Stop();
-                    musicPlaying = false;
-                    allObjectsInactive = false;
-                }
-                return;
-            }
-        }
+    public bool isMuted = false;
 
-        if (allObjectsInactive && !musicPlaying)
-        {
-            audioSource.Play();
-            musicPlaying = true;
-        }
-        else if (!allObjectsInactive && musicPlaying)
+    
+    public void Update()
+    {
+        if (videoScreen.activeSelf)
         {
             audioSource.Stop();
-            musicPlaying = false;
+        }
+        else
+        {
+            audioSource.Play();
         }
     }
 
     public void Mute()
     {
-
-        if (isMuted)
+        if (audioSource.isPlaying)
         {
             audioSource.Stop();
         }
-        else
+        else if (!audioSource.isPlaying)
         {
             audioSource.Play();
         }

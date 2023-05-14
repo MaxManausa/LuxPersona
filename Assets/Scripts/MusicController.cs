d4976ab1f9125d120089635d38d310c8f47b833c
Max-Manausa <maxmanausa@gmail.com>
2023-05-14 18:34:57 -0400

    It works! This is the ARVR Build for Quest 2!

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -7,20 +7,6 @@ public class MusicController : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private GameObject videoScreen;
     
-    public bool isMuted = false;
-
-    
-    public void Update()
-    {
-        if (videoScreen.activeSelf)
-        {
-            audioSource.Stop();
-        }
-        else
-        {
-            audioSource.Play();
-        }
-    }
 
     public void Mute()
     {
